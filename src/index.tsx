423bd9269e4802556cf6ab83e2ddf7042e2eba19
srajoo <rajoo@usc.edu>
2023-01-17 09:33:40 -0800

    commit

--- src/index.tsx
+++ src/index.tsx
@@ -19,7 +19,7 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Provider store={store}>
       <App />
     </Provider>
